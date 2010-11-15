368efe6c396d2c9d2a43dbaaf769bb6af750fd32
Tomás Senart <tsenart@me.com>
2010-11-15 03:44:01 +0000

    Working version; TODO: Change from Socket.io to Websocket-node-server

--- bitbasketd.js
+++ bitbasketd.js
@@ -1,137 +1,89 @@
 var
-  sys = require('sys'),
+  sys  = require('sys'),
   path = require('path'),
-  url = require('url'),
+  url  = require('url'),
   http = require('http'),
-  fs = require('fs'),
+  fs   = require('fs'),
   mime = require('mime'),
-  ws = require('websocket-server'),
-  decode = require('base64').decode,
-  qs = require('querystring')
-    
-  clients = {},
-  PORT = 80,
-  WEBROOT = path.join(path.dirname(__filename), 'public');
+  io   = require('socket.io'),
+  qs   = require('querystring'),
+  uuid = require('uuid'),
+  redis = require('redis'),
+      
+  PORT = 3000,
+  WEBROOT = path.join(path.dirname(__filename), 'public'),
+  DB = redis.createClient();
+
+var server = http.createServer(function(req, res) {
+  var pathname = url.parse(req.url).pathname; 
+  if(pathname == '/') pathname = '/index.html';
+  var filename = path.join(process.cwd(), 'public', qs.unescape(pathname));
+  var statuscode = 200;
   
-
-function randomstring(L){
-  var s= '';
-  var randomchar = function() {
-    var n= Math.floor(Math.random()*62);
-    if(n<10) return n; //1-10
-    if(n<36) return String.fromCharCode(n+55); //A-Z
-    return String.fromCharCode(n+61); //a-z
-  }
-  while(s.length< L) s += randomchar();
-  return s;
-}
-
-var httpServer = http.createServer(function(req, res) {
-  var uri = url.parse(req.url).pathname;  
-  var filename = path.join(process.cwd(), 'public', qs.unescape(uri));
   path.exists(filename, function(exists) {  
-    if(!exists) {  
-      res.writeHead(404, {"Content-Type": "text/plain"});  
-      res.end("404 Not Found\n");  
-      return;  
-    }  
+    if(!exists) {
+      statuscode = 404;
+      filename = '404.html';
+    }
 
-    fs.readFile(filename, "binary", function(err, file) {  
-      if(err) {  
-        res.writeHead(500, {"Content-Type": "text/plain"});  
-        res.end(err + "\n"); 
-        return;  
-      }  
-
-      res.writeHead(200, {"Content-Type": mime.lookup(filename)});  
-      res.write(file, "binary");  
-      res.end();  
+    fs.readFile(filename, function(err, file) {  
+      if(err) {
+        res.writeHead(500, {'Content-Type': 'text/plain'});  
+        res.end(err + '\n'); 
+        return;
+      }
+      res.writeHead(statuscode, {'Content-Type': mime.lookup(filename)});  
+      res.end(file);
     });  
   });
 });
 
-var server = ws.createServer({
-  server: httpServer
-});
+var socket = io.listen(server);
 
-server.addListener("listening", function(){
-  var pathname = path.join(process.cwd(), 'public/files');
-  path.exists(pathname, function(exists) { 
-    if(!exists)
-      fs.mkdir(pathname, 0777)
-  });
-})
-
-server.addListener("connection", function(conn) {
-  var bit = randomstring(12)
-  var pathname = path.join(process.cwd(), 'public/files', bit);
-  fs.mkdir(pathname, 0777)
-  for(var k in clients) {
-    fs.readdir(path.join(process.cwd(), 'public/files', clients[k].bit), function(err, files) {
-      if(!err && files.length > 0) {
-        for(var i = 0; i < files.length; ++i) {
-          conn.write(JSON.stringify({success:true, bit: clients[k].bit, name: files[i], coords: clients[k]['files'][files[i]]}))
-        }
-      }
-      else {
-        conn.write(JSON.stringify({success:false}))
-      }
-    })
-  }
-  clients[conn.id] = {bit: bit, files: {}};
-  
-  conn.addListener("message", function(data){
-    var file = JSON.parse(data)
-    var pathname = path.join(process.cwd(), 'public/files', clients[conn.id].bit, file.name);
-    path.exists(pathname, function(exists) {
-      if(!exists) {
-        // sys.puts(file.data)
-        // sys.puts("\n")
-        // sys.puts(decode(file.data))
-        fs.writeFile(pathname, decode(file.data), function (err) {
-          if (err) {
-            conn.write(JSON.stringify({success:false}))
-          } else {
-            console.log(file.cords)
-            if(!clients[conn.id]['files'][file.name])
-              clients[conn.id]['files'][file.name]= file.coords;
-            console.log('Saved "' + file.name + '" from ' + conn.id + ':' + clients[conn.id].bit);
-            server.broadcast(JSON.stringify({success:true, bit: clients[conn.id].bit, name: file.name, coords: file.coords}))
-          }
-        });
-      }
-      else {
-        conn.write(JSON.stringify({success:false}))
-      }
- 
-    })
-  })
-  
-  conn.addListener("close", function() {
-    path.exists(pathname, function(exists) { 
-      if(exists) {
-        fs.rmdir(pathname, function() {
-          server.broadcast(JSON.stringify({success:true, bit: clients[conn.id].bit}))
-          delete clients[conn.id];
-        });
-      }
-    });
-  });
-});
-
-process.on('exit', function () {
-  server.emit("shutdown")
-})
-
-server.addListener("shutdown", function(){
-  var pathname = path.join(process.cwd(), 'public/files');
-  path.exists(pathname, function(exists) { 
-    if(exists) {
-      fs.rmdir(pathname, function() {
-        server.broadcast(JSON.stringify({success:true}))
-        delete clients;
+socket.on('connection', function(client) {
+  var id = uuid.generate();
+  client.send(JSON.stringify({uuid: id}));
+  DB.keys('*', function(err, keys) {
+    if(err || !keys) return;
+    for(var i in keys) {
+      var key = keys[i].toString('utf8');
+      DB.smembers(key, function(err, data) {
+        if(err || !data) return;
+        for(var j in data) {
+          var bit = JSON.parse(data[j].toString('utf8'));
+          client.send(JSON.stringify({
+            uuid: key,
+            file: {
+              name: bit.file.name,
+              size: bit.file.size
+            },
+            coords: bit.coords
+          }));
+        }        
       });
     }
+  });
+  
+  client.on('message', function(data) {
+    var msg = JSON.parse(data);
+    if(msg.uuid && msg.file && msg.coords)
+      DB.sadd(msg.uuid, JSON.stringify({file: msg.file, coords: msg.coords}));
+    client.broadcast(JSON.stringify({
+      uuid: msg.uuid,
+      file: {
+        name: msg.file.name,
+        size: msg.file.size,
+      },
+      coords: msg.coords
+    }));
+  });
+  
+  client.on('disconnect', function() {
+    DB.del(id);
+    client.broadcast(JSON.stringify({
+      uuid: id,
+      del: true
+    }));
   });
 });
 
